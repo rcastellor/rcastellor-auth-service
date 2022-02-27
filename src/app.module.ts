15002f80e036921556e9fa938679cb7b27790d09
Roberto Castellor <roberto.castellor@gmail.com>
2022-02-27 15:03:41 +0100

    [WIP] Refactor context folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,8 @@ import { CqrsModule } from '@nestjs/cqrs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NestCommandBusService } from './shared/infrastructure/nest-command-bus.service';
-import { AuthModule } from './modules/auth/auth.module';
-import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './contexts/auth/auth.module';
+import { UserModule } from './contexts/user/user.module';
 
 @Module({
   imports: [CqrsModule, AuthModule, UserModule],
