1e3c7e559754da9e9208e7cf1038e8c0f802293f
Roberto Castellor <roberto.castellor@gmail.com>
2022-03-01 13:33:34 +0100

    [SONARQUBE] Code smell

--- src/contexts/auth/infrastructure/controllers/signout.controller.ts
+++ src/contexts/auth/infrastructure/controllers/signout.controller.ts
@@ -1,8 +1,5 @@
 import { Controller, Inject, Post, Request, Res } from '@nestjs/common';
 import { ITokenRepository } from '../../domain/token.repository';
-import { Refresh } from '../../application/refresh.service';
-import { JwtService } from '@nestjs/jwt';
-import { AuthToken } from '../../domain/auth-token.entity';
 import { Signout } from '../../application/signout.service';
 
 @Controller('signout')
@@ -12,7 +9,7 @@ export class SignoutController {
 
     @Post()
     async signout(@Request() req, @Res() res) {
-        const signout = await new Signout(this.tokenRepository)
+        await new Signout(this.tokenRepository)
             .execute(req.cookies['TE-refresh-token']);
         res.clearCookie('TE-refresh-token');
         res.send();
